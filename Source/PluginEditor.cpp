87f35015daa2abb1f9964c91bc74fd5a3bb88786
imABEING <kamitchell@berklee.edu>
2019-01-05 20:10:09 -0600

    A Variable Delay Line Plugin made with the JUCE Framework

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,63 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+SimpleDelayAudioProcessorEditor::SimpleDelayAudioProcessorEditor (SimpleDelayAudioProcessor& p, AudioProcessorValueTreeState& vts)
+    : AudioProcessorEditor (&p), processor (p), valueStateTree (vts)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (600, 400);
+    
+    //Delay Time
+    numDelayDial.setSliderStyle(Slider::RotaryVerticalDrag);
+    numDelayDial.setRange(20.0, 1000.0, 1);
+    numDelayDial.setValue(500.0);
+    numDelayDial.setNumDecimalPlacesToDisplay(0);
+    numDelayDial.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 50);
+    numDelayDial.setTextValueSuffix(" Hz");
+    addAndMakeVisible(numDelayDial);
+    
+    //Delay Label
+    addAndMakeVisible(numDelayLabel);
+    numDelayLabel.setText("Cutoff", dontSendNotification);
+    numDelayLabel.setJustificationType(Justification::centred);
+    numDelayValue = new AudioProcessorValueTreeState::SliderAttachment(valueStateTree, "delayValue", numDelayDial);
+    
+}
+
+SimpleDelayAudioProcessorEditor::~SimpleDelayAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void SimpleDelayAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setColour (Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Variable Delay Line Stereo Reverb", getLocalBounds(), Justification::centred, 1);
+}
+
+void SimpleDelayAudioProcessorEditor::resized()
+{
+    const int labelWidth = getWidth() / 2;
+    const int labelHeight = 25;
+    const int sliderWidth = getWidth() / 2;
+    const int sliderHeight = getHeight() - labelHeight;
+    
+    
+    numDelayDial.setBounds(20, 20, sliderWidth, sliderHeight);
+}
---
+++ Source/PluginProcessor.h
@@ -0,0 +1,70 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/**
+*/
+class SimpleDelayAudioProcessor  : public AudioProcessor
+{
+public:
+    //==============================================================================
+    SimpleDelayAudioProcessor();
+    ~SimpleDelayAudioProcessor();
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
+
+    //==============================================================================
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+    
+    void fillDelayBuffer (int channel, const int bufferLength, const int delayBufferLength, const float* bufferData, const float* delayBufferData);
+    void getFromDelayBuffer (AudioBuffer<float>& buffer, int channel, const int bufferLength, const int delayBufferLength, const float* bufferData, const float* delayBufferData);
+    void feedbackDelay (int channel, const int bufferLength, const int delayBufferLength, float* dryBuffer);
+    
+    AudioProcessorValueTreeState tree;
+    
+private:
+    AudioBuffer<float> mDelayBuffer;
+    int mWritePosition {0};
+    int mSampleRate {44100};
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleDelayAudioProcessor)
+};
